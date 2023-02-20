9f7e19eec9b27228ebfe5c18c5e7e470cc987468
FerRamBCS <74386601+FerRamBCS@users.noreply.github.com>
2023-02-19 20:38:11 -0700

    VERSION 1.3.2
    OPCION AGREGAR A SIDO CREADA Y PROBADA

--- prototipo_Proyecto/php/insertar.php
+++ prototipo_Proyecto/php/insertar.php
@@ -4,7 +4,6 @@ $getmysql = new mysqlconex();
 $getconex = $getmysql->conex();
 
 	if (isset($_POST["registrar"])) {
-		$fecha = $_POST["fecha_Venta"];
 		$nombre = $_POST["nombre"];
 		$colonia = $_POST["colonia"];
 		$referencia = $_POST["referencia"];
@@ -12,9 +11,9 @@ $getconex = $getmysql->conex();
 		$volumen = $_POST["volumen"];
 		$total = $_POST["total"];
 
-		$query = "INSERT INTO sistema_ventas (Fecha,Cliente,Colonia,Referencias,Material,Volumen,Total) VALUES(?,?,?)";
+		$query = "INSERT INTO  sistema_ventas (Nombre,Colonia,Referencias,Material,Volumen,Total) VALUES(?,?,?,?,?,?)";
 		$sentencia = mysqli_prepare($getconex,$query);
-		mysqli_stmt_bind_param($sentencia,"sssd",$fecha,$nombre,$colonia,$referencia,$material,$volumen,$total);
+		mysqli_stmt_bind_param($sentencia,"ssssdd",$nombre,$colonia,$referencia,$material,$volumen,$total);
 		mysqli_stmt_execute($sentencia);
 		$afectado = mysqli_stmt_affected_rows($sentencia);
 		if ($afectado == 1) 
