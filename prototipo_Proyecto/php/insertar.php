7d977ebc3a447fe7ba04c4cbecb59ca6f18b29b9
FerRamBCS <74386601+FerRamBCS@users.noreply.github.com>
2023-02-20 08:40:33 -0700

    VERSION 1.4
    OPCION SELECT HA SIDO IMPLEMENTADA Y PROBADA

--- prototipo_Proyecto/php/insertar.php
+++ prototipo_Proyecto/php/insertar.php
@@ -3,7 +3,8 @@ include("conexion.php");
 $getmysql = new mysqlconex();
 $getconex = $getmysql->conex();
 
-	if (isset($_POST["registrar"])) {
+	if (isset($_POST["registrar"])) 
+	{
 		$nombre = $_POST["nombre"];
 		$colonia = $_POST["colonia"];
 		$referencia = $_POST["referencia"];
@@ -18,10 +19,10 @@ $getconex = $getmysql->conex();
 		$afectado = mysqli_stmt_affected_rows($sentencia);
 		if ($afectado == 1) 
 		{
-			echo "<script>alert('El pedido de [$nombre] ha sido registrado'); location.href='../index.html';</script>";
+			echo "<script>alert('El pedido de [$nombre] ha sido registrado'); location.href='../index.php';</script>";
 		}else 
 		{
-			echo "<script>alert('El pedido de [$nombre] no pudo ser registrado'); location.href='../index.html';</script>";
+			echo "<script>alert('El pedido de [$nombre] no pudo ser registrado'); location.href='../index.php';</script>";
 		}
 		mysqli_stmt_close($sentencia);
 		mysqli_close($getconex);
